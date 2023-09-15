bf092b48b9fad5e63cc79c8199490bab310a0064
Your Name <you@example.com>
2023-09-15 10:55:05 +0700

    first

--- resources/js/layouts/Nav.vue
+++ resources/js/layouts/Nav.vue
@@ -54,7 +54,7 @@
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                            <img src="" alt="User Avatar" class="img-size-50 mr-3 img-circle">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     Brad Diesel

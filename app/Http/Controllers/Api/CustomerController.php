316147590f74a1a64cc06e87df7d63b3ada723a5
Your Name <you@example.com>
2023-09-16 15:52:29 +0700

    123'

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -67,8 +67,11 @@ class CustomerController
         return response([
             'customers' => Customer::query()->select('id', 'name', 'phone')
             ->when($request->input('search'), function ($query, $search) {
+                $slug_name =  \Str::slug($search);
                 $query->where('name', 'like', '%' . $search . '%')
-                    ->OrWhere('phone', 'like', '%' . $search . '%');
+                    ->OrWhere('phone', 'like', '%' . $search . '%')
+                    ->OrWhere('slug', 'like', '%' . $slug_name . '%');
+
             })->get()
         ], Response::HTTP_OK);
     }

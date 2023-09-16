16b74bb2a7ccd7181850b404a624cec8c2e39dfa
Your Name <you@example.com>
2023-09-17 01:37:27 +0700

    fix

--- resources/js/layouts/Nav.vue
+++ resources/js/layouts/Nav.vue
@@ -41,7 +41,7 @@
                 </a>
             </li>
 
-            <li class="nav-item" title="Kiểm tra tiền về" @click="checkAllCommand">
+            <li class="nav-item" title="Kiểm tra tiền" @click="checkAllCommand">
                 <a class="nav-link" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-terminal-split" viewBox="0 0 16 16">
                     <path d="M2.146 3.146a.5.5 0 0 1 .708 0l.823.824a.75.75 0 0 1 0 1.06l-.823.824a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708ZM4 6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1A.5.5 0 0 1 4 6Zm6.354-2.854a.5.5 0 0 0-.708.708l.647.646-.647.646a.5.5 0 1 0 .708.708l.823-.824a.75.75 0 0 0 0-1.06l-.823-.824ZM12 5.5a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1Z"/>

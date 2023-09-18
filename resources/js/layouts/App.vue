f13565a45f77cc9c4563d18d4ddbe30c2c954b56
Your Name <you@example.com>
2023-09-18 14:20:25 +0700

    123

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -2,6 +2,4 @@
     <router-view></router-view>
 </template>
 <script setup>
-import Nav  from './Nav.vue'
-import Sidebar  from './Sidebar.vue'
 </script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,89 +1,89 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
-// import User from '../pages/users/User.vue'
-// import Customer from '../pages/customers/Customer.vue'
-// import CustomerPantner from '../pages/customers/CustomerPantner.vue'
-// import CustomerCard from '../pages/customerCard/CustomerCard.vue'
-// import Drawals from '../pages/drawal/Drawal.vue'
-// import WithDrawals from '../pages/withDrawal/WithDrawal.vue'
-// import Transactions from '../pages/transaction/Transactions.vue'
-// import Expenses from '../pages/expense/Expense.vue'
-// import Globals from '../pages/global/Globals.vue'
-// import GlobalDetails from '../pages/global/GlobalDetails.vue'
-// import DashboardPos from '../pages/dashboardPos/DashboardPos.vue'
-// import Historys from '../pages/history/History.vue'
+import Home from '../views/Home.vue'
+import User from '../pages/users/User.vue'
+import Customer from '../pages/customers/Customer.vue'
+import CustomerPantner from '../pages/customers/CustomerPantner.vue'
+import CustomerCard from '../pages/customerCard/CustomerCard.vue'
+import Drawals from '../pages/drawal/Drawal.vue'
+import WithDrawals from '../pages/withDrawal/WithDrawal.vue'
+import Transactions from '../pages/transaction/Transactions.vue'
+import Expenses from '../pages/expense/Expense.vue'
+import Globals from '../pages/global/Globals.vue'
+import GlobalDetails from '../pages/global/GlobalDetails.vue'
+import DashboardPos from '../pages/dashboardPos/DashboardPos.vue'
+import Historys from '../pages/history/History.vue'
 
 import Login from '../layouts/Login.vue'
 
 const routes =  [
-    // {
-    //     path: "/",
-    //     component: Home,
-    //     children :[
-    //         {
-    //             path:"/user",
-    //             name: "User",
-    //             component: User
-    //         }
-    //         ,
-    //         {
-    //             path:"/customer",
-    //             name: "Customer",
-    //             component: Customer
-    //         },
-    //         {
-    //             path:"/customer-cards",
-    //             name: "CustomerCard",
-    //             component: CustomerCard
-    //         },
-    //         {
-    //             path:"/drawals",
-    //             name: "Drawals",
-    //             component: Drawals
-    //         }
-    //         ,
-    //         {
-    //             path:"/withdrawals",
-    //             name: "WithDrawals",
-    //             component: WithDrawals
-    //         },
-    //         {
-    //             path:"/transactions",
-    //             name: "Transaction",
-    //             component: Transactions
-    //         },
-    //         {
-    //             path:"/expenses",
-    //             name: "Expense",
-    //             component: Expenses
-    //         },
-    //         {
-    //             path:"/history",
-    //             name: "History",
-    //             component: Historys
-    //         },
-    //         {
-    //             path:"/dashboard-pos",
-    //             name: "DashboardPos",
-    //             component: DashboardPos
-    //         },
-    //         {
-    //             path:"/globals",
-    //             name: "Global",
-    //             component: Globals
-    //         },
-    //         {
-    //             path:"/global-details",
-    //             name: "GlobalDetail",
-    //             component: GlobalDetails
-    //         },
-    //         {
-    //             path:"/customer-partners",
-    //             name: "CustomerPantner",
-    //             component: CustomerPantner
-    //         }
-    //     ]
-    // },
+    {
+        path: "/",
+        component: Home,
+        children :[
+            {
+                path:"/user",
+                name: "User",
+                component: User
+            }
+            ,
+            {
+                path:"/customer",
+                name: "Customer",
+                component: Customer
+            },
+            {
+                path:"/customer-cards",
+                name: "CustomerCard",
+                component: CustomerCard
+            },
+            {
+                path:"/drawals",
+                name: "Drawals",
+                component: Drawals
+            }
+            ,
+            {
+                path:"/withdrawals",
+                name: "WithDrawals",
+                component: WithDrawals
+            },
+            {
+                path:"/transactions",
+                name: "Transaction",
+                component: Transactions
+            },
+            {
+                path:"/expenses",
+                name: "Expense",
+                component: Expenses
+            },
+            {
+                path:"/history",
+                name: "History",
+                component: Historys
+            },
+            {
+                path:"/dashboard-pos",
+                name: "DashboardPos",
+                component: DashboardPos
+            },
+            {
+                path:"/globals",
+                name: "Global",
+                component: Globals
+            },
+            {
+                path:"/global-details",
+                name: "GlobalDetail",
+                component: GlobalDetails
+            },
+            {
+                path:"/customer-partners",
+                name: "CustomerPantner",
+                component: CustomerPantner
+            }
+        ]
+    },
     {
         path:"/login",
         name: "Login",

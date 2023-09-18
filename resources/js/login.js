d6e5d4c64a63621d0c77f3c6eb02c0be1bb9d6f2
Your Name <you@example.com>
2023-09-18 14:28:16 +0700

    Update

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
-import router from './router/index.js'
 import App from './layouts/Login.vue'
 import '../plugins/fontawesome-free/css/all.min.css'
 import '../dist/css/adminlte.min.css'
-
-import '../plugins/jquery/jquery.min.js'
 
 import Axios from 'axios'
 window.axios = Axios;
 
 let app = createApp(App)
-app.use(router)
 app.mount('#app')

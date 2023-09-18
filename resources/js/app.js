5453c59a3f5c785f29e8440592961c58e313cc0a
Your Name <you@example.com>
2023-09-18 14:49:33 +0700

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,35 @@
 import { createApp } from 'vue'
-import App from './layouts/Login.vue'
-import '../plugins/fontawesome-free/css/all.min.css'
-import '../dist/css/adminlte.min.css'
+import router from './router/index.js'
+import App from './layouts/App.vue'
 
-import Axios from 'axios'
-window.axios = Axios;
+import '../plugins/fontawesome-free/css/all.min.css'
+import  '../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'
+import  '../plugins/datatables-responsive/css/responsive.bootstrap4.min.css'
+import  '../plugins/datatables-buttons/css/buttons.bootstrap4.min.css'
+
+import '../dist/css/adminlte.min.css'
+import 'vue3-easy-data-table/dist/style.css'
+import  '../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css'
+import "vue-select/dist/vue-select.css";
+
+import '../plugins/jquery/jquery.min.js'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+import '../dist/css/customize.css'
+
+import 'vue3-toastify/dist/index.css'
+import Vue3EasyDataTable from 'vue3-easy-data-table'
+import { createPinia } from 'pinia'
+import VueSelect  from "vue-select";
+import VueNumberFormat from 'vue-number-format'
+
 let app = createApp(App)
+app.use(VueNumberFormat, {prefix: '', decimal: '.', thousand: '.,'})
+app.use(router)
+
+app.use(createPinia())
+app.component('EasyDataTable', Vue3EasyDataTable)
+app.component("v-select", VueSelect)
 app.mount('#app')
